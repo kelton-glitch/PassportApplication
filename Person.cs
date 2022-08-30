807324db9fa9a97dfca2d7d55e3a694515f431ac
Kamgo N Elton <kamgoelton89@gmail.com>
2022-08-30 12:13:19 +0100

    initiating getters and setters in Person class

--- Person.cs
+++ Person.cs
@@ -22,8 +22,112 @@ namespace PassportApplication
         private string guardianAddress;
         private int guardianPhone;
 
-        private string oldPassportNumber;
+        private int oldPassportNumber;
         private string passportIssueDate;
         private string passportExpiryDate;
+
+        //PERSONAL INFORMATION
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { emailAddress = value; }
+        }
+
+        public int PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string HomeAddress
+        {
+            get { return homeAddress; }
+            set { homeAddress = value; }
+        }
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public string Occupation
+        {
+            get { return occupation; }
+            set { occupation = value; }
+        }
+
+        public string DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+
+        //GUARDIAN INFORMATION
+
+        public string GuardianName
+        {
+            get { return guardianName; }
+            set { guardianName = value; }
+        }
+
+        public string GuardianAddress
+        {
+            get { return guardianAddress; }
+            set
+            {
+                guardianAddress = value;
+            }
+        }
+
+        public int GuardianPhone
+        {
+            get { return guardianPhone; }
+            set { guardianPhone = value; }
+        } 
+
+
+        //OLO PASSPORT INFORMATION
+
+        public int PassportNumber
+        {
+            get { return oldPassportNumber; }
+            set { oldPassportNumber = value; }
+        }
+
+        public string PassportIssueDate
+        {
+            get { return passportIssueDate; }
+            set { passportIssueDate = value; }  
+        }
+
+        public string PassportExpiryDate
+        {
+            get { return passportExpiryDate; }
+            set
+            {
+                passportExpiryDate = value;
+            }   
+        }
+    
     }
 }
